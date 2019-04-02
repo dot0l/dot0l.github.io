3c59d2dc37d1a03fe50966bbddbc39dc138bed35
trueboy012 <ivancuevas1@hotmail.com>
2019-04-02 01:25:57 -0400

    firstrelease

---
+++ final/js/base.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){/*prepara las funciones para el documento actual
+    MUY IMPORTANTE, si esta linea no se include el jquery no funcionara*/
+    $("#bloque-izq").click(function(){
+        $("#pan-izq").slideToggle(500); //hace un slide de .5s al panel
+      });
+    $("#bloque-der").click(function(){
+        $("#pan-der").slideToggle(500); //hace un slide de .5s al panel
+      });
+});
